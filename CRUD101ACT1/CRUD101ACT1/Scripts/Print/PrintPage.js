36589737cd8d01e66772075ad00126705ddc8aca
MSI\Jeff <leoloujeffrey.dev@gmail.com>
2024-07-31 22:30:06 +0800

    this is the first commit: uploading CRUD101 folder into leoloujeffreydev/ljdev-crud101 repository

---
+++ CRUD101ACT1/CRUD101ACT1/Scripts/Print/PrintPage.js
@@ -0,0 +1,29 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+
+
+    //--------------GET THE ELEMENTS FROM THE HTML DOCUMENT--------------//
+    var btnPrintPage = document.getElementById('btnPrintPage');
+    var PrintPatientInfoForm = document.getElementById('PrintPatientInfoForm');
+
+    //--------------FUNCTION--------------//
+    function printPage() {
+        window.print();
+    }
+
+    function printDiv(divId) {
+        var printContents = document.getElementById(divId).innerHTML;
+        var originalContents = document.body.innerHTML;
+
+        document.body.innerHTML = printContents;
+
+        window.print();
+
+        document.body.innerHTML = originalContents;
+    }
+
+    btnPrintPage.addEventListener('click', function () { //button is click and submit function
+       // printPage();
+        printDiv(divId);
+    });
+
+});
---
+++ CRUD101ACT1/CRUD101ACT1/Scripts/ViewPatient/ViewPatient.js
@@ -0,0 +1,50 @@
+﻿//--------------DOM--------------//
+document.addEventListener('DOMContentLoaded', function () {
+
+    //--------------GET THE ELEMENTS FROM THE HTML DOCUMENT--------------//
+
+
+    //--------------VALIDATION MESSAGES--------------//
+
+
+    //--------------FUNCTIONS--------------//
+
+
+    //--------------FUNCTION FOR TESTING PURPOSES--------------//
+
+    //--------------MODAL--------------//
+
+    function showModal() {
+        $('#apModal').modal('show');
+    }
+
+    function tryModal() {
+        let txtPName = document.getElementById('txtPName');
+        let aptxtPNameval = document.getElementById('txtPName').value;
+
+        if (aptxtPNameval == "m") {
+            $('#apModal').modal('show');
+            txtPName.value = "mm";
+        }
+        else {
+            txtPName.value = "not mm";
+        }
+    }
+
+    //--------------ADDING AN EVENT LISTENER TO THE BUTTON--------------//
+
+    //btnSubmit.addEventListener('click', validateForm);
+    btnTry.addEventListener('click', tryModal);
+    //btnSubmit.addEventListener('click', valPName, valDrug, valDosage);
+
+    //--------------PREVENT FORM SUBMISSION--------------//
+    document.getElementById('addPatientForm').addEventListener('submit', function (event) {
+        if (!valPName() || !valDrug() || !valDosage()) { // if not validateform
+            event.preventDefault(); // prevent form submission if validation fails
+        }
+
+    });
+
+
+    //END DOM
+});
